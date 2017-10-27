d7836d011fefccc0580e832c7181c8e2944cbce2
Alison10515002 <peggy989500@gmail.com>
2017-10-27 14:34:12 +0800

    1

--- httpd/index.js
+++ httpd/index.js
@@ -3,18 +3,19 @@
 let http = require('http');
 
 http.createServer((request, response) => {
-// 傳送 HTTP header
-// HTTP Status: 200 : OK
+ // 取得 node.js 的 fs 模組
+let fs = require('fs')
 // Content Type: text/plain
-response.writeHead(200, {
-'Content-Type': 'text/plain'
-});
+fs.readFile('../htdocs/index.html', (err, data) => {
+  response.writeHead(200,  {
+     'Content-Type': 'text/html'
+   });
 
-// 傳送回應內容。
-response.end('Hello World!\n');
+ response.write(data);
 
-console.log('request.headers: \n', request.headers)
-}).listen(8088);
+ response.end();
+ });
+  }).listen(8088);
 
 // log message to Console
-console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+console.log(' 伺服器啟動，連線 url: http://127.0.0.1:8088/'); 
