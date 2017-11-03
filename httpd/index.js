b53df13a1e26e6a2e08773846dd6b4aa48537a26
Alison10515002 <32382480+Alison10515002@users.noreply.github.com>
2017-11-03 20:44:41 +0800

    11/3  02

--- httpd/index.js
+++ httpd/index.js
@@ -3,17 +3,21 @@
 let http = require('http');
 
 http.createServer((request, response) => {
-// 傳送 HTTP header
-// HTTP Status: 200 : OK
-// Content Type: text/plain
-response.writeHead(200, {
-'Content-Type': 'text/plain'
-});
+  request.on('end', () => {
+    console.log('Request method: ' + request.method);
+    console.log('Request url: ' + request.url);
+  });
 
-// 傳送回應內容。
-response.end('Hello World!\n');
+  // 傳送 HTTP header
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
 
-console.log('request.headers: \n', request.headers)
+  // 傳送回應內容。
+  response.end('Hello World!\n');
 }).listen(8088);
+
 // log message to Console
-console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+console.log('Server running at http://127.0.0.1:8088/');
