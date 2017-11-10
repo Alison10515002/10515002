3a1fb302e92257e768a558ad36c5d8b7a333fceb
Alison10515002 <peggy989500@gmail.com>
2017-11-10 15:08:19 +0800

    11/10 000

--- httpd/index.js
+++ httpd/index.js
@@ -8,12 +8,12 @@ const routingTable = {
     type: 'text/html'
   },
 
-  '/assets/css/styles.css': {
+  '/styles.css': {
 
     url: '../htdocs/assets/css/styles.css',
     type: 'text/css'
   },
-  '/assets/png/SokobanClone_byVellidragon.png': {
+  '/SokobanClone_byVellidragon.png': {
     url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
     type: 'image/png'
   },
