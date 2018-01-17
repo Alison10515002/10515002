cd2e523caa005283b9629acfca909620162ac2b6
Alison10515002 <peggy989500@gmail.com>
2018-01-17 16:31:10 +0800

    1/17

--- htdocs/js/sokoban.js
+++ htdocs/js/sokoban.js
@@ -565,8 +565,7 @@ let sokoban = {
   */
   update: function (e) {
     this.move(e);
-    if(putBoxOnGoal: function ({x, y}) {
-      this.level[y] = replaceAt(this.level[y], x, SOKOBAN.BOX_ON_GOAL){
+    if(this.level[y] = replaceAt(this.level[y], x, SOKOBAN.BOX_ON_GOAL){
     alert('恭喜!!你過關了~');}
     this.paint();
     //
