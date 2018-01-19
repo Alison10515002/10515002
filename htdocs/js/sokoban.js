4ea18b010da406132b7f034d5a25bb7cc51b0bcd
Alison10515002 <peggy989500@gmail.com>
2018-01-19 20:47:23 +0800

    43

--- htdocs/js/sokoban.js
+++ htdocs/js/sokoban.js
@@ -402,7 +402,7 @@ let prototypeGameState = {
     this.level[y] = replaceAt(this.level[y], x, SOKOBAN.BOX_ON_GOAL);
     if (true) {
       alert('恭喜!!你過關了~');}
-      else　if (this.putBoxOnGoal==NULL){
+      else　if (this.Goal==NULL){
         alert('請繼續努力!革命尚未成功^v^');
       }
     return this;
