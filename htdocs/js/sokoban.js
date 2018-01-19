530a7ff7c68a618e2457e66c73aeb6bd2b1fce51
Alison10515002 <peggy989500@gmail.com>
2018-01-19 19:37:50 +0800

    21

--- htdocs/js/sokoban.js
+++ htdocs/js/sokoban.js
@@ -565,7 +565,7 @@ let sokoban = {
   */
   update: function (e) {
     this.move(e);
-    if putBoxOnGoal(ALL true) {
+    if (this.isBox(this.level[y].charAt(x) == SOKOBAN.BOX_ON_GOAL) {
       alert('恭喜!!你過關了~');}
       else {
         alert('請繼續努力!革命尚未成功^v^');
